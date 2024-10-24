c482a47372e4b1e4c71a5406f37de230fc9abc1d
Satyam9993 <satyamkatiyar9993@gmail.com>
2024-10-24 21:44:19 +0530

    creating public and private route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Login from './pages/LoginAndRegister.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import ProtectedRoute from './utils/ProtectedRoute.jsx';
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
     <>
-    <Login/>
+    {/* <Login/> */}
+    <Router>
+      <Routes>
+        {/* Public Pages */}
+        <Route path="/login" element={<Login setAuth={setIsAuthenticated} />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
+              <HomePage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
     </>
   )
 }
