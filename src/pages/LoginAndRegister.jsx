eaaac13b0d889bd1e92b98713581edd0b24b42e3
Satyam9993 <satyamkatiyar9993@gmail.com>
2024-10-24 21:12:58 +0530

    creating login and logut page

---
+++ src/pages/LoginAndRegister.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm.jsx';
+import Register from '../components/Register';
+
+const LoginAndRegister = () => {
+    const token = false;
+    return (
+        <>
+            <div className="bg-yellow-300 flex justify-center items-center h-screen">
+                <div className="w-1/2 h-screen hidden lg:block">
+                    <img src="https://img.freepik.com/free-vector/tablet-login-concept-illustration_114360-7883.jpg?t=st=1729782666~exp=1729786266~hmac=ae07c5ca8f665f8b72ebef2c603f4551068044f6dd15bc4df9ca1112f13aa9b4&w=740" alt="Placeholder Image" className="object-cover w-full h-full" />
+                </div>
+                {token == false ?    
+                    <LoginForm /> : <Register/>
+                }
+            </div>
+        </>
+    )
+}
+
+export default LoginAndRegister
